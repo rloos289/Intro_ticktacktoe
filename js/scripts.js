1b933940d1af5c1d738fad17eeb57357b167e15a
Ben Vissotzky and Ryan Loos <ben@>
2016-08-24 11:11:36 -0700

    create basic html, connect html to objects

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,10 @@
 //
 // }
 //
+
+
+
+
 function generateBoard (array) {
 
 }
@@ -23,7 +27,14 @@ function generateSpaces (a,b) {
       spaceArray.push(newSpace = new space (false, x, y));
     }
   }
+
   return spaceArray;
+}
+
+function arrangeHTML () {
+  var next = 0
+  next ++;
+  $('.game').append("<div class='space" + next + "'></div>");
 }
 
 function space (marked, x, y) {
@@ -32,7 +43,29 @@ function space (marked, x, y) {
   this.spacey = y
 }
 
+
+
+
+// shit that is useless
+// function fuck() {
+//   spaceindex = 1
+//   for (var ridx = 1; ridx <= 3; ridx++) {
+//     $('.game').append("<div class='row" + ridx + "'></div>");
+//     for (var cidx = 1; cidx <= 3; cidx++) {
+//       debugger;
+//       // $('.game'+ (' row'+ ridx)).append("<div class='col-xs-4 space" + spaceindex  + "'></div>");
+//       console.log('.game'+ (' row'+ ridx))
+//       spaceindex++;
+//     }
+//   }
+// }
+
 //<!-- Front End  -->
 $(document).ready(function(){
-
+  $('.space').click(function() {
+    var id = parseInt($(this).attr('id'));
+    var array = generateSpaces(3,3);
+    array[id-1].mark = true;
+    console.log(array[id-1]);
+  });
 });
